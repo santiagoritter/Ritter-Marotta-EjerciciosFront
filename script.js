128f640583fa23b28b41d65d3b4196121968b401
Santiago Ritter <santiagoritter@gmail.com>
2026-04-11 13:29:46 -0300

    sitio web portfolio

---
+++ script.js
@@ -0,0 +1,6 @@
+const boton = document.getElementById('boton_menu');
+const nav = document.getElementById('navegacion');
+
+boton.addEventListener('click', function() {
+    nav.classList.toggle('abierto');
+});
